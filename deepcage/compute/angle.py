1767c440851b8ceb2826fc9542fe9cd085a7f251
caniko2 <canhtart@gmail.com>
2019-10-27 18:21:30 +0100

    non-sessile angle change wip

--- deepcage/compute/angle.py
+++ deepcage/compute/angle.py
@@ -4,7 +4,7 @@ import numpy as np
 from .utils import *
 
 
-def non_sessile_angle_change(p1, p2):
+def non_sessile_angle_change(rp, p):
     """
     Parameters
     ----------
@@ -12,6 +12,17 @@ def non_sessile_angle_change(p1, p2):
     p2 : numpy.array-like
     """
 
-    line_unit_v = np.where(p1 - p2
+    rp_smaller_idxs = np.where(norm(rb) < norm(p))
+    line_vector_smaller = p[rp_smaller_idxs] - rp[rp_smaller_idxs]
 
-    baseline = np.median(line_unit_v)
+    rp_larger_idxs = np.where(norm(rb) > norm(p))
+    line_vector_larger = rp[rp_larger_idxs] - p[rp_larger_idxs]
+
+    line_vectors = np.zeros((len(rb), 3))
+    line_vectors[rp_smaller_idxs] = line_vector_smaller
+    line_vectors[rp_larger_idxs] = line_vector_larger
+    line_units = unit(line_vectors)
+
+    baseline_unit_vector = np.median(line_units, axis=0)
+    # baseline_uv_idx = line_units.index(np.percentile(line_units, 50, interpolation='nearest'))
+    
